0d188280c11dc5058f161b2915032166847e432d
Silviu Anton <silviu.c.anton@gmail.com>
2018-11-07 15:45:37 +0200

    Documentatie functii
    Am documentat functiile din controller-e.

--- controller/client_controller.py
+++ controller/client_controller.py
@@ -1,7 +1,7 @@
 '''
 Created on Nov 6, 2018
 
-Modul pentru gestiunea clientilor
+Modul pentru gestionarea clientilor.
 
 @author: Silviu Anton
 '''
@@ -14,26 +14,73 @@ class ClientController:
         self.__nextClient_ID = 1
     
     def get_all(self):
+        '''
+        Description: returneaza o lista cu toti clientii din repository-ul de clienti
+        '''
         return list(self.__repository.values())
     
     def get_client(self, ID):
+        '''
+        Description: returneaza clientul cu ID-ul dat
+        
+        In:
+            - ID - numar intreg
+        
+        Out:
+            - client
+        '''
         return self.__repository[ID]
     
     def add_client(self, firstName, lastName, CNP):
+        '''
+        Description: audauga un client in repository-ul de clienti, alocandu-i un ID
+        
+        In:
+            - firstName - prenumele clientului
+            - lastName - nuemele clientului
+            - CNP - codul numeric personal al clientului
+        '''
         client = Client(self.__nextClient_ID, firstName, lastName, CNP)
         self.__nextClient_ID += 1
         self.__repository[client.getID()] = client  
         
     def delete_client(self, ID):
+        '''
+        Description: sterge un client din repository-ul de clienti
+        '''
         self.__repository.pop(ID)
     
     def modify_client(self, ID, firstName, lastName, CNP):
+        '''
+        Description: modifica datele clientului cu ID-ul dat
+        
+        In:
+            - ID - id-ul clientului caruia ii modificam datele
+            - firstName - prenume
+            - lastName - nume
+            - CNP - cod numeric personal
+        '''
         self.__repository[ID].setName(firstName, lastName)
         self.__repository[ID].setCNP(CNP)
         
     def modify_client_name(self, ID, firstName, lastName):
+        '''
+        Description: modifica numele unui clinet
+        
+        In:
+            - ID - id-ul clientului caruia ii modificam datele
+            - firstName - prenume
+            - lastName - nume
+        '''
         self.__repository[ID].setName(firstName, lastName)
     
     def modify_client_CNP(self, ID, CNP):
+        '''
+        Description: modifica numele unui clinet
+        
+        In:
+            - ID - id-ul clientului caruia ii modificam datele
+            - CNP - cod numeric personal
+        '''
         self.__repository[ID].setCNP(CNP)
         
--- controller/movie_controller.py
+++ controller/movie_controller.py
@@ -1,7 +1,7 @@
 '''
 Created on Nov 6, 2018
 
-Modul pentru gestiunea filmelor
+Modul pentru gestionarea filmelor
 
 @author: Silviu Anton
 '''
@@ -14,27 +14,71 @@ class MovieController:
         self.__nextMovieID = 1
     
     def get_all(self):
+        '''
+        Descriprion: returneaza o lista cu toate filmele din repository-ul de filme
+        '''
         return list(self.__repository.values())
     
     def add_movie(self, title, description, genre):
+        '''
+        Description: adauga un film in repository-ul de filme, alocand-ui un ID
+        
+        In:
+            - title - titlul filmului
+            - description - descrierea filmului
+            - genre - genul filmului
+        '''
         movie = Movie(self.__nextMovieID, title, description, genre)
         self.__nextMovieID +=1
         self.__repository[movie.getID()] = movie 
         
     def delete_movie(self, ID):
+        '''
+        Description: sterge un film din repository-ul de filme
+        '''
         self.__repository.pop(ID)
         
     def modify_movie(self, ID, title, description, genre):
+        '''
+        Description: modifica datele unui film
+        
+        In:
+            - ID - id-ul filmului caruia ii modificam datele
+            - title - titlu
+            - description - descriere
+            - genre - gen
+        '''
         self.__repository[ID].setTitle(title)
         self.__repository[ID].setGenre(genre)
         self.__repository[ID].setDescription(description)
         
     def modify_movie_genre(self, ID, genre):
+        '''
+        Description: modifica genul unui film
+        
+        In:
+            - ID - id-ul filmului caruia ii modificam datele
+            - genre - gen
+        '''
         self.__repository[ID].setGenre(genre)
         
     def modify_movie_description(self, ID, description):
+        '''
+        Description: modifica descrierea unui film
+        
+        In:
+            - ID - id-ul filmului caruia ii modificam datele
+            - description - descriere
+        '''
         self.__repository[ID].setDescription(description)
         
     def modify_movie_title(self, ID, title):
+        '''
+        Description: modifica titlul unui film
+        
+        In:
+            - ID - id-ul filmului caruia ii modificam datele
+            - title - titlu
+        '''
         self.__repository[ID].setTitle(title)
         
